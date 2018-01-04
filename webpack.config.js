c64bbf874d896e713654ac7b11f4a826d482a825
yorong <tpgml6824@gmail.com>
2018-01-04 17:50:01 +0900

    modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,48 @@
-
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname + "/src",
-  // devtool: debug ? "inline-sourcemap" : "#source-map",
-  devtool: debug ? "inline-sourcemap" : false,
-  entry: {
-    app: "./js/App.js"
-  },
+  entry: './src/index.js',
+
+  // devtool: "#eval-source-map",
+
   output: {
-    path: __dirname + "/src/",
-    filename: "bundle.js",
-    // sourceMapFilename: "bundle.map"
+    path: __dirname + '/dist/',
+    filename: 'bundle.js'
   },
+
+  devServer: {
+    inline: true,
+    port: 7777,
+    contentBase: __dirname + '/dist/'
+  },
+
   module: {
     loaders: [
       {
-        test: /\.js?$/,
+        test:/\.js$/,
+        loader: 'babel-loader',
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
         query: {
+          cacheDirectory: true,
           presets: ["react", "es2015", "stage-0"],
           plugins: ["react-html-attrs", "transform-class-properties", "transform-decorators-legacy"]
         }
       }
     ]
   },
+
+  plugins: [],
+
   plugins: debug ? [] : [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.OccurrenceOrderPlugin()
+    // new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
     // new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: true }),
-  ],
+  ]
+
+  // resolve: {
+  //   modules: [node_modules],
+  //   extensions: ['.js', '.json', '.jsx', '.css']
+  // },
+
 };
