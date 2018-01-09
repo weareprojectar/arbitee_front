063f4150756b8a19f7cb9823c45a13770d6d188c
jhjo <isidol11@hotmail.com>
2018-01-09 15:21:02 +0900

    webpack.config 에 style-loader, css-loader connect and make some index file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Home from './Home/index'
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <h1>hello world!</h1>
+      <Home/>
     );
   }
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class Home extends Component {
+    render() {
+      return (
+        <div className="Head">AR</div>
+      );
+    }
+  }
+  
+  
+  export default Home;
+  
+
