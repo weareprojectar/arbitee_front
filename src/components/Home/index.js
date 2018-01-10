2718a4e3d08e2550835466d138eeb5a5e7b3b9f0
jhjo <isidol11@hotmail.com>
2018-01-10 20:47:07 +0900

    Home_v1

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,11 +3,23 @@ import './index.css';
 
 class Home extends Component {
     render() {
-      return (
-        <div className="Head">AR</div>
-      );
+        return (
+            <body>
+            <div id="wrap">
+                <header class="header">
+                    <nav>AR</nav>
+                </header>
+                <section class="slider_section">
+                    <button class="btn1">시작하기</button>
+                    <button class="btn2">로그인</button>
+                    <button class="btn3">회원가입</button>
+                </section>
+                <footer class="footer"></footer>
+            </div>
+        </body>
+        )
     }
-  }
+}
   
   
   export default Home;
